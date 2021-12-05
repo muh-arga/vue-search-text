4cb33eaa3e2c3edf31c366ffb569ce78ad7f92cb
= <=>
2021-12-05 21:40:35 +0800

    fix api

--- src/api/textList.js
+++ src/api/textList.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const textList = (url, text, page) => {
   const baseUrl = "https://cari-teks-video-api.vercel.app/api/search";
 
-  return axios.post(baseUrl, {
+  return axios.get(baseUrl, {
     params: {
       url: url,
       q: text,
